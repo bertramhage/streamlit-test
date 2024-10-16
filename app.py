8f81c87f4ddda868fc39bcef4a162a15709b27f6
bertramhage <b.hage@me.com>
2024-10-16 21:51:31 +0200

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Hello World!')
 
 st.write('This is a simple example of a Streamlit app.')
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+st.write('Here is a DataFrame:')
+
+st.write(df)
